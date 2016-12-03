c0c1c4d5a88057e657a017dd5b523022cfe610ff
Trevor Vaughan <trevor-vaughan@users.noreply.github.com>
2016-12-02 20:29:43 -0500

    sysctl should not be silent by default

--- manifests/kvm.pp
+++ manifests/kvm.pp
@@ -7,7 +7,7 @@
 # * Trevor Vaughan <tvaughan@onyxpoint.com>
 #
 class libvirt::kvm {
-  include 'libvirt'
+  include '::libvirt'
 
   exec { 'kvm_mod_check':
     command => '/usr/local/sbin/loadkvm.rb',
@@ -22,8 +22,8 @@ class libvirt::kvm {
     source => 'puppet:///modules/libvirt/loadkvm.rb'
   }
 
-  if $::operatingsystem in ['RedHat', 'CentOS'] {
-    $package_list = $::operatingsystemmajrelease ? {
+  if $facts['operatingsystem'] in ['RedHat', 'CentOS'] {
+    $package_list = $facts['operatingsystemmajrelease'] ? {
       '7' => [
         'ipxe-roms',
         'ipxe-roms-qemu',
@@ -44,7 +44,7 @@ class libvirt::kvm {
     }
   }
   else {
-    warning("${::operatingsystem} not yet supported. Current options are RedHat and CentOS")
+    warning("${facts['operatingsystem']} not yet supported. Current options are RedHat and CentOS")
   }
   package { $package_list:
     ensure => 'latest',
@@ -54,33 +54,28 @@ class libvirt::kvm {
   # Enable Forwarding
   sysctl { 'net.ipv4.conf.all.forwarding':
     ensure => 'present',
-    val    => '1',
-    silent => true
+    val    => '1'
   }
   sysctl { 'net.ipv4.ip_forward':
     ensure => 'present',
-    val    => '1',
-    silent => true
+    val    => '1'
   }
   
   # Bypass the base hosts's IPTables
   sysctl { 'net.bridge.bridge-nf-call-arptables':
     ensure => 'present',
-    val    => '0',
-    silent => true
+    val    => '0'
   }
   sysctl { 'net.bridge.bridge-nf-call-iptables':
     ensure => 'present',
-    val    => '0',
-    silent => true
+    val    => '0'
   }
 
   # TODO: Make native boolean when we use facter 2.0
-  if $::ipv6_enabled == true {
+  if $facts['ipv6_enabled'] == true {
     sysctl { 'net.bridge.bridge-nf-call-ip6tables': 
       ensure => 'present',
-      val    => '0',
-      silent => true
+      val    => '0'
     }
   }
   else {
