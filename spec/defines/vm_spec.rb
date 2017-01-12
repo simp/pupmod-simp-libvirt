edcce93d10730ec3186af534c7380355c2fd0b14
Dylan Cochran <dylan.cochran@onyxpoint.com>
2017-01-11 19:47:45 -0500

    (SIMP-2465) Fix a bug in the newvm.erb template
    The newvm.erb template in pupmod-simp-libvirt has some variable scoping
    issues in both 5.X and 6.X. Per our older naming scheme, any variable
    that starts with t_ is a local variable and should not be scoped
    globally with @.
    
    Add more comprehensive tests to check for regressions.
    
    Add the gitlabci.yml while I'm here
    
    SIMP-2465 #close

--- spec/defines/vm_spec.rb
+++ spec/defines/vm_spec.rb
@@ -1,5 +1,4 @@
 require 'spec_helper'
-
 describe 'libvirt::vm' do
 
   let(:title) {'foo_vm'}
@@ -7,12 +6,44 @@ describe 'libvirt::vm' do
     :operatingsystemmajrelease => '6',
     :operatingsystem   => 'CentOS'
   }}
-  let(:params) {{
-    :size       => 50,
-    :pxe        => true,
-    :target_dir => '/tmp/test_libvirt'
-  }}
+  {
+    "default" => {
+      :size       => 50,
+      :pxe        => true,
+      :target_dir => '/tmp/test_libvirt'
+    },
+    "larger_disk_size" => {
+      :size       => 100,
+      :pxe        => true,
+      :target_dir => '/tmp/test_libvirt'
+    },
+    "specify_disk_opts" => {
+      :size => 100,
+      :disk_opts       => { 'bus' => '1', 'format' => 'qcow', 'io' => 'test'},
+        :pxe        => true,
+        :target_dir => '/tmp/test_libvirt'
+    },
+    "increased_vcpus" => {
+      :vcpus       => 4,
+      :size => 50,
+      :pxe        => true,
+      :target_dir => '/tmp/test_libvirt'
+    },
+    "larger_memory_size" => {
+      :mem       => 1024,
+      :size     => 50,
+      :pxe        => true,
+      :target_dir => '/tmp/test_libvirt'
+    }
 
-  it { is_expected.to create_file('/tmp/test_libvirt').with_ensure('directory') }
-  it { is_expected.to create_file('/usr/local/sbin/vm-create-foo_vm.sh').with_content(/size=50/) }
+  }.each do |name, hash|
+    context "when test case = #{name}" do
+      let(:params) {
+        hash
+      }
+      it { is_expected.to create_file('/tmp/test_libvirt').with_ensure('directory') }
+      create_vm = File.open("#{File.dirname(__FILE__)}/data/#{name}.txt", "rb").read;
+      it { is_expected.to create_file('/usr/local/sbin/vm-create-foo_vm.sh').with_content(create_vm) }
+    end
+  end
 end
